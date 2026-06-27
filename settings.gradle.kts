b3b7a2eb23c1ceb5137abd7b93fb54367241ef4a
Rizky_CodeCreator.ID <165538119+rizkyfi72id@users.noreply.github.com>
2026-06-27 17:17:11 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,49 @@
+rootProject.name = "HikmahTune"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":androidApp")
+include(":desktopApp")
+include(":shared")
+
+// Metrolist ported modules
+include(":innertube")
+include(":kugou")
+include(":lrclib")
+include(":lastfm")
+include(":betterlyrics")
+include(":shazamkit")
+include(":paxsenix")
